7ade9b9c892b25fe80253a3aff90bdfae21770dc
Jackson Henry <jackson.henry13@yahoo.com>
2019-05-29 14:47:22 -0500

    Avoiding DRY in MySQL file. Manipulating the JavaScript file so that I don't get the same result in st ock_quantity.

--- bAmazon.sql
+++ bAmazon.sql
@@ -13,34 +13,16 @@ CREATE TABLE products(
 );
 
 INSERT INTO products(item_id, product_name, category, price, stock_quantity)
-VALUES(1, "Basketball", "Sports Equipment", 15, 50);
-
-INSERT INTO products(item_id, product_name, category, price, stock_quantity)
-VALUES(2, "Slow Cooker", "Kitchenware", 30, 10);
-
-INSERT INTO products(item_id, product_name, category, price, stock_quantity)
-VALUES(3, "Vacuum Cleaners", "Appliances", 80, 20);
-
-INSERT INTO products(item_id, product_name, category, price, stock_quantity)
-VALUES(4, "Air Mattress", "Bedding", 100, 25);
-
-INSERT INTO products(item_id, product_name, category, price, stock_quantity)
-VALUES(5, "Dog Harness", "Pets", 20.95, 37);
-
-INSERT INTO products(item_id, product_name, category, price, stock_quantity)
-VALUES(6, "Wet Dry Vacuum", "Tools", 80.77, 16);
-
-INSERT INTO products(item_id, product_name, category, price, stock_quantity)
-VALUES(7, "Body Spray", "Personal Care", 30, 50);
-
-INSERT INTO products(item_id, product_name, category, price, stock_quantity)
-VALUES(8, "50 Inch Flatscreen TV", "Entertainment", 189.99, 6);
-
-INSERT INTO products(item_id, product_name, category, price, stock_quantity)
-VALUES(9, "Gas Grill", "Home Decor", 159.99, 8);
-
-INSERT INTO products(item_id, product_name, category, price, stock_quantity)
-VALUES(10, "Multi Day Backpack", "Camping Gear", 49.99, 12); 
+VALUES(1, "Basketball", "Sports Equipment", 15, 50),
+(2, "Slow Cooker", "Kitchenware", 30, 10),
+(3, "Vacuum Cleaners", "Appliances", 80, 20),
+(4, "Air Mattress", "Bedding", 100, 25),
+(5, "Dog Harness", "Pets", 20.95, 37),
+(6, "Wet Dry Vacuum", "Tools", 80.77, 16),
+(7, "Body Spray", "Personal Care", 30, 50),
+(8, "50 Inch Flatscreen TV", "Entertainment", 189.99, 6),
+(9, "Gas Grill", "Home Decor", 159.99, 8),
+(10, "Multi Day Backpack", "Camping Gear", 49.99, 12); 
 
 SELECT * FROM products;
 
